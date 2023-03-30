936b08dff9e316f29e353ef35852501eb475dbf5
001mbilal <muhammadbilal2702164@gmail.com>
2023-03-30 17:42:01 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { INITIAL_EVENTS, createEventId } from "./event-utils";
 
 export default function DemoApp() {
   const [weekendsVisible, setWeekendsVisible] = useState(true);
